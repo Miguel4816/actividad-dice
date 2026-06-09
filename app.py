0257b27b1ba8f7e265ed7dc688101f96de15ab3f
Miguel4816 <miguelmateo05@gmail.com>
2026-06-09 00:57:55 -0400

    mi segundo commit con el codigo de dados

---
+++ app.py
@@ -0,0 +1,17 @@
+import time
+import dice
+
+def lanzar_dados(amount, sides):
+    formula = f"{amount}d{sides}"
+    resultados = dice.roll(formula)
+    return resultados
+
+amount = 5
+sides = 6
+
+resultados_obtenidos = lanzar_dados(amount, sides)
+
+for indice, resultado in enumerate(resultados_obtenidos, start=1):
+    print(f"Lanzamiento {indice} número obtenido {resultado}")
+    if indice < amount: 
+        time.sleep(5) 
